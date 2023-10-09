6e486bb658c7da0349dbb81ce76fa05d55f2a40f
unknown <SelinN@lcoast.itstep>
2023-10-09 21:01:21 +0600

    JSON

--- views/views/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.contrib import admin
-
-urlpatterns = [
-    path('', include('views_app.urls')),
-    path('admin/', admin.site.urls),
-    path('captcha/', include('captcha.urls')),
-    path('accounts/', include('accounts.urls'))
-]
